4093cd759dd3e6f66366190e57328729f9617898
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-08-22 14:31:51 +0200

    mid vid 9

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -9,22 +9,47 @@ import {
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from '@material-ui/core';
 
 export default class HomePage extends Component {
   constructor(props) {
     super(props);
   }
 
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" component="h3">
+            Music Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to='/join' component={ Link }>
+              Join a Room
+            </Button>
+            <Button color="secondary" to='/create' component={ Link }>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
   render() {
-    return <Router>
-      <Switch>
-        <Route exact path='/' >
-          <p>This is the home page</p>
-        </Route>
-        <Route path='/join' component={RoomJoinPage} />
-        <Route path='/create' component={CreateRoomPage} />
-        <Route path='/room/:roomCode' component={Room} />
-      </Switch>
-    </Router>;
+    return (
+      <Router>
+        <Switch>
+          <Route exact path='/' >
+            {this.renderHomePage()}
+          </Route>
+          <Route path='/join' component={RoomJoinPage} />
+          <Route path='/create' component={CreateRoomPage} />
+          <Route path='/room/:roomCode' component={Room} />
+        </Switch>
+      </Router>
+    );
   }
 }
