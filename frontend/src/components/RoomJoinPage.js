5be55a5b605613cff6e1aa58b894ab73cefdd3fd
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-08-22 14:15:23 +0200

    [VIDEO] end of video#8

--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,75 @@
 import React, { Component } from 'react';
+import { TextField, Button, Grid, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default class RoomJoinPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      roomCode: "",
+      error: "",
+    }
+    this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+    this.roomButtonPressed = this.roomButtonPressed.bind(this);
   }
 
   render() {
-    return <p>This is the join room page</p>
+    return (
+      <Grid container spacing={1} align="center">
+        <Grid item xs={12}>
+          <Typography variant="h4" component="h4">
+            Join a Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            error={this.state.error}
+            label="Code"
+            placeholder="Enter a Room Code"
+            value={this.state.roomCode} // state
+            helperText={this.state.error}
+            variant="outlined"
+            onChange={this.handleTextFieldChange}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" color="primary" onClick={this.roomButtonPressed}>
+            Enter Room
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" color="secondary" to="/" component={Link}>
+            {/* Link makes the button act as a Link (this comes from react-router-dom) */}
+            Back
+          </Button>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  handleTextFieldChange(e) {
+    // _ indicates it's a private function (makes no functional difference, just code readability)
+    this.setState({
+      roomCode: e.target.value,
+    });
+  }
+
+  roomButtonPressed() { // we don't need any args because we're sending a POST request
+    const requestOptions = {
+      method: "POST",
+      headers: {"Content-Type": "application/json" },
+      body: JSON.stringify({
+        code: this.state.roomCode
+      })
+    };
+    fetch('/api/join-room', requestOptions).then((response) => {
+      if (response.ok) {
+        this.props.history.push(`/room/${this.state.roomCode}`);
+      } else {
+        this.setState({error: "Room not found."});
+      }
+    }).catch((error) => {
+      console.log(error);
+    })
   }
 }
