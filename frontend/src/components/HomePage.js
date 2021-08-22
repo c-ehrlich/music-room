384ce8b93b1c61f5d095f7fcc57f73a22363db33
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-08-22 17:40:26 +0200

    [VIDEO] end of video#10

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -17,6 +17,7 @@ export default class HomePage extends Component {
     this.state = {
       roomCode: null,
     };
+    this.clearRoomCode = this.clearRoomCode.bind(this);
   }
 
   async componentDidMount() { // componentDidMount usually doesn't require async, but in our case it does
@@ -52,6 +53,12 @@ export default class HomePage extends Component {
     );
   }
 
+  clearRoomCode() {
+    this.setState({
+      roomCode: null,
+    });
+  }
+
   render() {
     return (
       <Router>
@@ -69,7 +76,12 @@ export default class HomePage extends Component {
           />
           <Route path='/join' component={RoomJoinPage} />
           <Route path='/create' component={CreateRoomPage} />
-          <Route path='/room/:roomCode' component={Room} />
+          <Route
+            path='/room/:roomCode'
+            render={(props) => {
+              return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+            }}
+          />
         </Switch>
       </Router>
     );
