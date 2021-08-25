60a8af4d162a58af61696be5e6eebd7b73230c9d
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2021-08-25 12:21:22 +0200

    add models to admin page

--- spotify/admin.py
+++ spotify/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from spotify.models import SpotifyToken
+from spotify.models import SpotifyToken, Vote
 
 # Register your models here.
 admin.site.register(SpotifyToken)
+admin.site.register(Vote)
